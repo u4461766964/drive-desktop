2b05b3f4eed3946a8babe05e28f87c3db0ea2d18
u4461766964 <92992750+u4461766964@users.noreply.github.com>
2026-06-28 11:28:49 +0100

    Delete src/apps/main/antivirus directory

--- src/apps/main/antivirus/antivirus-manager/types.ts
+++
@@ -1,5 +0,0 @@
-import { AntivirusClamAV } from '../antivirus-clam-av';
-import { AntivirusWindowsDefender } from '../windows-defender/antivirus-windows-defender';
-
-export type AntivirusType = 'windows-defender' | 'clamav';
-export type AntivirusEngine = AntivirusClamAV | AntivirusWindowsDefender;
